ce53abd1622460bd5dec0d0ca393ca6dd5389516
yanweifree <yanweifree@163.com>
2013-07-06 19:15:00 +0800

    static pages test

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -1,6 +1,7 @@
 require 'spec_helper'
 
 describe "Static pages" do
+
   describe "Home page" do
     it "should have the content 'Sample App'" do
       # Run the generator again with the --webrat flag if you want to use webrat methods/matchers
@@ -8,4 +9,21 @@ describe "Static pages" do
       page.should have_content('Sample App')
     end
   end
+
+  describe "Help page" do
+    it "should have the content 'Help'" do
+      # Run the generator again with the --webrat flag if you want to use webrat methods/matchers
+      visit '/static_pages/help'
+      page.should have_content('Help')
+    end
+  end
+
+    describe "About page" do
+    it "should have the content 'About Us'" do
+      # Run the generator again with the --webrat flag if you want to use webrat methods/matchers
+      visit '/static_pages/about'
+      page.should have_content('About Us')
+    end
+  end
+
 end
